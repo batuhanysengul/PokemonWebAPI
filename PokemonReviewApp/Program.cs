f7a8ea3848dee21a06daa2209348c8470082355f
Batuhan <batuhanyigitsengul@gmail.com>
2023-09-16 22:38:08 +0300

    implemented Repository Pattern / Dependency Injection

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using PokemonReviewApp;
 using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Dependency Injection
+builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+
 
 
 builder.Services.AddDbContext<DataContext>(options =>
