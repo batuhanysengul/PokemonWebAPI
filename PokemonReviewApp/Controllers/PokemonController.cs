f912edba4959b3d09a039a743a685ad20b877cb2
Batuhan <batuhanyigitsengul@gmail.com>
2023-09-18 13:32:31 +0300

    pokemon get & read methods

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -6,24 +6,71 @@ namespace PokemonReviewApp.Controllers
 {
     [Route("api/controller")]
     [ApiController]
+
     public class PokemonController : Controller
     {
+
+        //repository starts
         private readonly IPokemonRepository _pokemonRepository;
 
         public PokemonController(IPokemonRepository pokemonRepository)
         {
             _pokemonRepository = pokemonRepository;
         }
+        //repository plugged in
 
+
+
+        //returns a list
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
         public IActionResult GetPokemons()
         {
             var pokemons = _pokemonRepository.GetPokemons();
-            
-            if(!ModelState.IsValid) 
+
+            if (!ModelState.IsValid)
                 return BadRequest(ModelState);
-                return Ok(pokemons);
+            return Ok(pokemons);
         }
+
+
+        //returns 1 tane pokemon 
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))          
+                return NotFound();
+
+            var pokemon = _pokemonRepository.GetPokemon(pokeId);
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemon);
+        }
+
+
+        [HttpPost("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int pokeId)
+            //IActionResult sayesinde NotFound - BadRequest - Ok gibi type'lar dönebiliyoruz.
+        {
+            if(!_pokemonRepository.PokemonExists(pokeId))
+                return NotFound();
+
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+
+            //ModelState = Validation nesnesi, model binding sırasında error var mı bakar
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok(rating);
+        }
+
+
+
     }
 }
---
+++ PokemonReviewApp/Dto/PokemonDto.cs
@@ -0,0 +1,13 @@
+﻿namespace PokemonReviewApp.Dto
+{
+    public class PokemonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        /*Dto - Api isteği atıldığında model'e yazdığımız her şeyi dönmek yerine
+        sadece istediğimiz belli bi kısmın dönmesini sağlar
+        aynı şekilde apiye gelecek veriler de sınırlandırılabilir dto ile*/
+    }
+}
