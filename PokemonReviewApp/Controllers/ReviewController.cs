9627bed8165fb40aa4300efd15082e8a31e1cdcd
Batuhan <batuhanyigitsengul@gmail.com>
2023-09-20 17:17:15 +0300

    review get & read methods

---
+++ PokemonReviewApp/Controllers/ReviewController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : Controller
+    {
+        //
+        private readonly IReviewRepository _reviewRepository;
+        public ReviewController(IReviewRepository reviewRepository)
+        {
+            _reviewRepository = reviewRepository;
+        }
+        //
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        public IActionResult GetReviews()
+        {
+            var reviews = _reviewRepository.GetReviews();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(reviews);
+        }
+
+        [HttpGet("{reviewId}")]
+        [ProducesResponseType(200, Type = typeof(Review))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId))
+                return NotFound();
+
+            var review = _reviewRepository.GetReview(reviewId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(review);
+        }
+
+        [HttpGet("pokemon/{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Review))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewsForAPokemon(int pokeId)
+        {
+            var review = _reviewRepository.GetReviewsOfAPokemon(pokeId);
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(review);
+        }
+
+    }
+
+
+}
