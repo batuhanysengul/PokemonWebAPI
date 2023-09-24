4424c15b6c2f59244c2f09773fdf654b06b2564e
Batuhan <batuhanyigitsengul@gmail.com>
2023-09-24 13:26:25 +0300

    category create + mapping profile changes

--- PokemonReviewApp/Controllers/CategoryController.cs
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -68,16 +68,16 @@ namespace PokemonReviewApp.Controllers
         [HttpPost]
         [ProducesResponseType(204)]
         [ProducesResponseType(400)]
-        public IActionResult CreateCategory([FromBody] Category categoryCreate)
+        public IActionResult CreateCategory([FromBody] CategoryDto categoryCreate)
         {
-            if(categoryCreate == null)
+            if (categoryCreate == null)
                 return BadRequest(ModelState);
 
             var category = _categoryRepository.GetCategories()
                 .Where(c => c.Name.Trim().ToUpper() == categoryCreate.Name.TrimEnd().ToUpper())
                 .FirstOrDefault();
 
-            if(category != null)
+            if (category != null)
             {
                 ModelState.AddModelError("", "Category already exists");
                 return StatusCode(422, ModelState);
@@ -92,11 +92,9 @@ namespace PokemonReviewApp.Controllers
             {
                 ModelState.AddModelError("", "Something went wrong while saving");
                 return StatusCode(500, ModelState);
-
             }
 
-            return Ok("Succesfully created");
-
+            return Ok("Successfully created");
         }
 
     }
